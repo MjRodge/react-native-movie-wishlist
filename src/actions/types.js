50ca01554ac59668e90f53c1764c5e688ef19e7d
Michael Rodgers <mail@mjrodge.me>
2019-02-05 17:15:58 -0500

    Added Auth Actions

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,12 @@
+//Movie action types
 export const GET_MOVIES_NEW_RELEASES = 'get_movies_new_releases';
 export const GET_MOVIES_NEW_RELEASES_SUCCESS =
   'get_movies_new_releases_success';
 export const GET_MOVIES_NEW_RELEASES_FAIL = 'get_movies_new_releases_fail';
+
+//Auth action types
+export const EMAIL_CHANGED = 'email_changed';
+export const PASSWORD_CHANGED = 'password_changed';
+export const LOGIN_USER_SUCCESS = 'login_user_success';
+export const LOGIN_USER_FAIL = 'login_user_fail';
+export const LOGIN_USER = 'login_user';
