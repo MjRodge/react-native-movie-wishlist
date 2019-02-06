85f5774313a57ce323a07134c2e2907953930730
Michael Rodgers <mail@mjrodge.me>
2019-02-06 15:01:04 -0500

    Added Infinite Scrolling Feature to MovieList

--- src/components/movieList.js
+++ src/components/movieList.js
@@ -7,16 +7,30 @@ import { getMoviesNewReleases } from '../actions';
 import MovieDetail from './movieDetail';
 
 class MovieList extends Component {
+  state = {
+    pageCount: 1,
+  };
+
   componentWillMount() {
-    this.props.getMoviesNewReleases();
+    this.props.getMoviesNewReleases(this.state.pageCount);
   }
+
+  loadMoreMoviesNewReleases = () => {
+    this.setState({ pageCount: this.state.pageCount + 1 }, () => {
+      console.log(this.state.pageCount);
+      this.props.getMoviesNewReleases(this.state.pageCount);
+      console.log(this.props.movies);
+    });
+  };
 
   render() {
     return (
       <FlatList
         data={this.props.movies}
         renderItem={({ item }) => <MovieDetail movie={item} />}
-        keyExtractor={item => item.id.toString()}
+        //keyExtractor={item => item.id.toString()}
+        onEndReached={this.loadMoreMoviesNewReleases}
+        onEndReachedThreshold={0.1}
       />
     );
   }
--- src/reducers/MovieListReducer.js
+++ src/reducers/MovieListReducer.js
@@ -11,11 +11,16 @@ const initial_state = {
 export default (state = initial_state, action) => {
   switch (action.type) {
     case GET_MOVIES_NEW_RELEASES:
-      console.log(action.payload);
+      //console.log(action.payload);
       return { ...state, loading: true };
     case GET_MOVIES_NEW_RELEASES_SUCCESS:
-      console.log(action.payload);
-      return { ...state, loading: false, movies: action.payload };
+      //console.log(action.payload);
+      return {
+        ...state,
+        loading: false,
+        movies: [...state.movies, ...action.payload],
+        //movies: action.payload,
+      };
     default:
       return state;
   }
