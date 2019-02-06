0cb96f4c90e5056e8050708985345db3981dbe43
Michael Rodgers <mail@mjrodge.me>
2019-02-06 12:19:24 -0500

    Made Class Based Component

--- src/components/movieDetail.js
+++ src/components/movieDetail.js
@@ -1,45 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View, Image } from 'react-native';
 import { Card, CardSection, Button } from './common';
 
-const MovieDetail = ({ movie }) => {
-  const {
-    original_title,
-    overview,
-    poster_path,
-    vote_average,
-    backdrop_path,
-  } = movie;
-  const {
-    headerContentStyle,
-    headerTextStyle,
-    imageStyle,
-    reviewTextStyle,
-  } = styles;
+class MovieDetail extends Component {
+  addMovieToWishlistButtonPress() {
+    const {
+      original_title,
+      overview,
+      poster_path,
+      vote_average,
+      backdrop_path,
+    } = this.props.movie;
+    console.log(this.props.movie);
+  }
+  render() {
+    const {
+      original_title,
+      overview,
+      poster_path,
+      vote_average,
+      backdrop_path,
+    } = this.props.movie;
+    const {
+      headerContentStyle,
+      headerTextStyle,
+      imageStyle,
+      reviewTextStyle,
+    } = styles;
 
-  return (
-    <Card>
-      <CardSection>
-        <Image
-          source={{ uri: 'https://image.tmdb.org/t/p/w342' + poster_path }}
-          style={imageStyle}
-        />
-      </CardSection>
-      <CardSection>
-        <View style={headerContentStyle}>
-          <Text style={headerTextStyle}>{original_title}</Text>
-          <Text style={reviewTextStyle}>{vote_average}/10</Text>
-          <Text>{overview}</Text>
-        </View>
-      </CardSection>
+    return (
+      <Card>
+        <CardSection>
+          <Image
+            source={{ uri: 'https://image.tmdb.org/t/p/w342' + poster_path }}
+            style={imageStyle}
+          />
+        </CardSection>
+        <CardSection>
+          <View style={headerContentStyle}>
+            <Text style={headerTextStyle}>{original_title}</Text>
+            <Text style={reviewTextStyle}>{vote_average}/10</Text>
+            <Text>{overview}</Text>
+          </View>
+        </CardSection>
 
-      <CardSection>
-        <Button>Add to Wishlist</Button>
-        <Button>Already Seen</Button>
-      </CardSection>
-    </Card>
-  );
-};
+        <CardSection>
+          <Button onPress={this.addMovieToWishlistButtonPress.bind(this)}>
+            Add to Wishlist
+          </Button>
+          <Button>Already Seen</Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
 
 const styles = {
   headerContentStyle: {
